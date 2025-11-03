ba9d43631b6f63726a45adeaa306611371f9a7ec
James Wolff <james.wolff0@gmail.com>
2025-11-02 23:45:22 -0600

    chore: rename PUBLISHABLE_KEY to ANON_KEY

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,23 +1,21 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-import { hasEnvVars } from "../utils";
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
 
-  // If the env vars are not set, skip middleware check. You can remove this
-  // once you setup the project.
-  if (!hasEnvVars) {
-    return supabaseResponse;
-  }
+  console.log("ENV VARS:", {
+    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  });
 
   // With Fluid compute, don't put this client in a global environment
   // variable. Always create a new one on each request.
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
